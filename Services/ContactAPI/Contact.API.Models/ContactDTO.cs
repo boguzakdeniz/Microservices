51413937f34968a02067e0c969f318ece28962a7
Bahri Oguz Akdeniz <Bahri.Akdeniz@sovos.com>
2022-10-01 03:43:03 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Models/ContactDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact.API.Models
+{
+    public class ContactDTO
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public String FullName  => $"{FirstName} {LastName}";
+    }
+}
